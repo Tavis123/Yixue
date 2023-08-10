d9393c4c7f235c25c02953b9d02acae5ec9aebb3
Qi Wu <1650574530@qq.com>
2023-08-11 07:20:24 +0800

    搭建了媒资管理模块

---
+++ yixue-media/yixue-media-service/src/main/java/com/yixue/media/mapper/MqMessageMapper.java
@@ -0,0 +1,15 @@
+package com.yixue.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yixue.media.model.entity.MqMessage;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageMapper extends BaseMapper<MqMessage> {
+
+}
